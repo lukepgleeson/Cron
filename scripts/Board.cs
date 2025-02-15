6c95879fe6e22018cd6a3e16ee91b68b12d4e727
lukegleeson <luke@glees.online>
2025-02-15 18:00:29 +1100

    Code Clean Up
    Signed-off-by: lukegleeson <luke@glees.online>

--- scripts/Board.cs
+++ scripts/Board.cs
@@ -1,36 +1,27 @@
 using Godot;
 using System;
-using System.Collections.Generic;
 
 public partial class Board : GridContainer
 {
-    
-    GridContainer boardGrid;
-    PackedScene slotScene = null;
-    List<Node> gridList;
-	
     public override void _Ready()
-	{
+    {
         this.Columns = SizeConstants.BOARD_COLUMNS;
-        gridList = new List<Node>();
-        while(this.GetChildCount() < Math.Pow(this.Columns,2)){
+        while (this.GetChildCount() < Math.Pow(this.Columns, 2))
+        {
             createSlot();
         }
-	}
-
-	public override void _Process(double delta)
-	{
-	}
-
-    public int getColumns() {
-        return this.Columns;
     }
 
-    private void createSlot() {
+    public override void _Process(double delta)
+    {
+    }
+
+    private void createSlot()
+    {
         ColorRect slot = new ColorRect();
-        float opacity = (this.GetChildCount()+(this.GetChildCount()/this.Columns))%2 == 0 ? 0.2f : 0.8f;
+        float opacity = (this.GetChildCount() + (this.GetChildCount() / this.Columns)) % 2 == 0 ? 0.2f : 0.8f;
         slot.Color = new Color(Colors.OrangeRed, opacity);
-        slot.CustomMinimumSize = new Vector2(20,20);
+        slot.CustomMinimumSize = new Vector2(20, 20);
         this.AddChild(slot);
     }
 }
--- scripts/Player.cs
+++ scripts/Player.cs
@@ -4,35 +4,43 @@ using System;
 public partial class Player : CharacterBody2D
 {
     private bool canMove = false;
-    Vector2 moveDirection = new Vector2(0,0);
+    Vector2 moveDirection = new Vector2(0, 0);
 
-    public override void _Process(double delta) {
+    public override void _Process(double delta)
+    {
         move();
     }
 
-    private void move() {
+    private void move()
+    {
 
         var up = new Vector2(0, -SizeConstants.SQUARE);
         var down = new Vector2(0, SizeConstants.SQUARE);
         var left = new Vector2(-SizeConstants.SQUARE, 0);
         var right = new Vector2(SizeConstants.SQUARE, 0);
 
-        if(Input.IsActionJustPressed("move_down")) {
+        if (Input.IsActionJustPressed("move_down"))
+        {
             moveDirection = down;
         }
-        if(Input.IsActionJustPressed("move_up")) {
+        if (Input.IsActionJustPressed("move_up"))
+        {
             moveDirection = up;
         }
-        if(Input.IsActionJustPressed("move_left")) {
+        if (Input.IsActionJustPressed("move_left"))
+        {
             moveDirection = left;
         }
-        if(Input.IsActionJustPressed("move_right")) {
+        if (Input.IsActionJustPressed("move_right"))
+        {
             moveDirection = right;
         }
-        if((Math.Abs((this.Position + moveDirection).X) > (SizeConstants.SQUARE * SizeConstants.BOARD_COLUMNS)/2) ||
-        (Math.Abs((this.Position + moveDirection).Y) > (SizeConstants.SQUARE * SizeConstants.BOARD_COLUMNS)/2)){
+        if ((Math.Abs((this.Position + moveDirection).X) > (SizeConstants.SQUARE * SizeConstants.BOARD_COLUMNS) / 2) ||
+        (Math.Abs((this.Position + moveDirection).Y) > (SizeConstants.SQUARE * SizeConstants.BOARD_COLUMNS) / 2))
+        {
             moveDirection = Vector2.Zero;
-        };
+        }
+        ;
 
         this.Position = this.Position + moveDirection;
         moveDirection = Vector2.Zero;
